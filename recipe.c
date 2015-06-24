22c190d7deeeac19e077c33018119d5c45adf78e
kmiyashita <kotaro.miyashita@miraclelinux.com>
2015-06-24 16:29:09 +0900

    課題6を実装した

--- recipe.c
+++ recipe.c
@@ -3,12 +3,22 @@
 
 #define MAX_LENGTH 50
 
+/*RECIPE構造体の定義*/
+struct RECIPE{
+             char *recipe_title;
+             int recipe_id;
+             };
+
 int main(int argc, char *argv[]) {
 	
 	FILE*	fp;
 	char		str[MAX_LENGTH];
 	int		id;
-	
+  int id_selected;
+  struct RECIPE recipe[255];
+
+  id_selected=atoi(argv[2]);	
+
 	if (argc < 2) {
 		printf("ファイル名が入力されていません。\n");
 		exit(EXIT_FAILURE);
@@ -21,9 +31,14 @@ int main(int argc, char *argv[]) {
 	
 	for (id = 1; fgets(str, MAX_LENGTH, fp); id++) {
 		printf("%d:%s", id, str);
+    recipe[id].recipe_id=id;
+    recipe[id].recipe_title=str;
 	}
-	
+
 	fclose(fp);
+
+		printf("Selected recipe is %d:%s", recipe[id_selected].recipe_id, recipe[id_selected].recipe_title);
+
 	
 	return 0;
 }
